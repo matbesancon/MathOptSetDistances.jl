573d1f1bacea90f9e5efe1fa3fbf10a575dd4cca
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-05-30 00:37:02 +0100

    normed epigraph distance

--- src/distances.jl
+++ src/distances.jl
@@ -15,14 +15,6 @@ Default distance function, implemented for all sets.
 struct DefaultDistance <: AbstractDistance end
 
 """
-    EpigraphViolationDistance
-
-Distance used when `v ∈ s` can be expressed as a single scalar function `f(v) ≤ 0`.
-The distance is expressed as `max(f(v), 0)`.
-"""
-struct EpigraphViolationDistance <: AbstractDistance end
-
-"""
     distance_to_set(distance_definition, v, s)
 
 Compute the distance of a value to a set.
@@ -34,3 +26,23 @@ with `T` of appropriate type for members of the set.
 function distance_to_set end
 
 distance_to_set(::AbstractDistance, v, s) = distance_to_set(DefaultDistance(), v, s)
+
+"""
+    EpigraphViolationDistance
+
+Distance used when `v ∈ s` can be expressed as a single scalar function `f(v) ≤ 0`.
+The distance is expressed as `max(f(v), 0)`.
+"""
+struct EpigraphViolationDistance <: AbstractDistance end
+
+"""
+    NormedEpigraphDistance{p}
+
+Distance used when `v ∈ s` can be expressed as a several functions `f_i(v) ≤ 0 ∀ i ∈ I`.
+The distance is expressed as `norm([max(f_i(v), 0) ∀ i ∈ I], n)` with `p` a norm value (2, 1, Inf).
+"""
+struct NormedEpigraphDistance{p} <: AbstractDistance end
+
+# _norm_set(::NormedEpigraphDistance{1}) = MOI.NormOneCone
+# _norm_set(::NormedEpigraphDistance{2}) = MOI.SecondOrderCone
+# _norm_set(::NormedEpigraphDistance{Inf}) = MOI.NormInfCone
