0b4cd725935cbaac8a6d9badd72958b188a54d4e
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-12-21 18:25:34 +0100

    remove comments

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -42,7 +42,7 @@ using Test
                 (MOD.DefaultDistance(), nothing),
                 (x, xb),
                 (s, nothing),
-                atol=10e-6,
+                atol=10e-5,
             )
         end
         @testset "SOC $n" begin
@@ -72,44 +72,14 @@ using Test
     end
 end
 
-# n = 3
-# x = randn(n)
-# f = MOD.projection_on_set
-# s = MOI.Zeros(n)
-# y = MOD.projection_on_set(MOD.DefaultDistance(), x, s)
-# yb = ChainRulesTestUtils.rand_tangent(y)
-# ȳ = yb
-# xb = ChainRulesTestUtils.rand_tangent(x)
-# xx̄s=[
-#     (MOD.DefaultDistance(), nothing),
-#     (x, xb),
-#     (s, nothing),
-# ]
-#
-# xs = first.(xx̄s)
-# accumulated_x̄ = last.(xx̄s)
-# y_ad, pullback = rrule(f, xs...)
-# y = f(xs...)
-# y_ad == y
-#
-# ∂s = pullback(ȳ)
-# ∂self = ∂s[1]
-# x̄s_ad = ∂s[2:end]
-# @test ∂self === NO_FIELDS  # No internal fields
-# x̄s_is_dne = accumulated_x̄ .== nothing
-#
-# fdm = FiniteDifferences.central_fdm(5, 1)
-#
-# x̄s_fd = ChainRulesTestUtils._make_j′vp_call(fdm, (xs...) -> f(xs...), ȳ, xs, x̄s_is_dne)
-# f2 = ChainRulesTestUtils._wrap_function(f, xs, x̄s_is_dne)
-# ignores = collect(x̄s_is_dne)
-
 @testset "rrules univariate" begin
-
     @testset "Scalar $ST" for ST in (MOI.EqualTo, MOI.LessThan, MOI.GreaterThan)
         for _ in 1:10
             s = ST(10 * randn())
             x = 10 * randn()
+            if isapprox(x, MOI.constant(s), atol=10e-5)
+                x *= 2
+            end 
             xb = ChainRulesTestUtils.rand_tangent(x)
             y = MOD.projection_on_set(MOD.DefaultDistance(), x, s)
             yb = ChainRulesTestUtils.rand_tangent(y)
