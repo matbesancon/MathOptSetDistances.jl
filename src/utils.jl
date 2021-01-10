503c1a637a8154b05e9494b8e76fc83d5b218f3b
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-10 18:53:30 +0100

    small changes on bissect

--- src/utils.jl
+++ src/utils.jl
@@ -1,33 +1,30 @@
 
-function _bisection(f, left, right; max_iters=500, tol=1e-14)
-    # STOP CODES:
-    #   0: Success (floating point limit or exactly 0)
-    #   1: Failure (max_iters without coming within tolerance of 0)
+"""
+    _bisection(f, left::T, right::T; max_iters=500, tol=1e-14) -> Union{Nothing, T}
 
+Finds the root `x, f(x) ≈ 0` in interval `[left, right]`, returns nothing if convergence fails.
+Errors if the interval is non-bracketing (sign of `f(left)` identical to sign of `f(right)`).
+"""
+function _bisection(f, left, right; max_iters=500, tol=1e-14)
     for _ in 1:max_iters
         f_left, f_right = f(left), f(right)
-        sign(f_left) == sign(f_right) && error("Interval became non-bracketing.")
+        signbit(f_left) == signbit(f_right) && error("Interval became non-bracketing.")
 
-        # Terminate if interval length ~ floating point precision (< eps())
+        # Terminate if interval length ~ eps()
         mid = (left + right) / 2
         if left == mid || right == mid
-            return mid, 0
+            return mid
         end
-
         # Terminate if within tol of 0; otherwise, bisect
         f_mid = f(mid)
         if abs(f_mid) < tol
-            return mid, 0
+            return mid
         end
-        if sign(f_mid) == sign(f_left)
+        if signbit(f_mid) == signbit(f_left)
             left = mid
-            continue
-        end
-        if  sign(f_mid) == sign(f_right)
+        elseif signbit(f_mid) == signbit(f_right)
             right = mid
-            continue
         end
     end
-
-    return nothing, 1
+    return nothing
 end
